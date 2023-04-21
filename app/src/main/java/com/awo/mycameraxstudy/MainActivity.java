b737ba21bb1d7804352e0cb05dc315b9460ccc49
yue li <379702893@qq.com>
2023-04-21 18:10:51 +0800

    feat : 改造demo适配镜子，增加yuv旋转及转成bitmap方法

--- app/src/main/java/com/awo/mycameraxstudy/MainActivity.java
+++ app/src/main/java/com/awo/mycameraxstudy/MainActivity.java
@@ -12,7 +12,13 @@ import androidx.camera.core.PreviewConfig;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.LightingColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,14 +29,18 @@ import android.util.Size;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
     private int REQUEST_CODE_PERMISSIONS = 10; //arbitrary number, can be changed accordingly
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
 
-    TextureView ttv;// 用以显示预览画面的的View组件
+    ImageView ttv;// 用以显示预览画面的的View组件
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
         // 第一步：设置预览组件的OnLayoutChangeListener（当预览组件的尺寸发生改变时的回调）
         ttv = findViewById(R.id.ttv_camera_preview);
-        ttv.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                // 尺寸变化时确保预览画面的方向始终正确
-                updateTransform();
-            }
-        });
+//        ttv.setRotationY(180);//镜像翻转
+//        ttv.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+//            @Override
+//            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+//                // 尺寸变化时确保预览画面的方向始终正确
+//                updateTransform();
+//            }
+//        });
         /*View的宽、高确定后，将在主线程执行run()方法，此处用来启动相机*/
         ttv.post(new Runnable() {
             @Override
@@ -63,8 +74,8 @@ public class MainActivity extends AppCompatActivity {
          * 预览
          */
         // 计算屏幕参数:宽、高 、屏幕高宽比、尺寸
-        int aspRatioW = ttv.getWidth(); // 预览View的宽
-        int aspRatioH = ttv.getHeight(); // 预览View的高
+        int aspRatioW = 640; // 预览View的宽
+        int aspRatioH = 480; // 预览View的高
         Rational asp = new Rational (aspRatioW, aspRatioH); // 屏幕高、宽比
         Size screen = new Size(aspRatioW, aspRatioH); // 屏幕尺寸
 
@@ -80,12 +91,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onUpdated(Preview.PreviewOutput output) {
                 // 需要移除父组件后重新添加View组件，固定写法
-                ViewGroup parent = (ViewGroup) ttv.getParent();
-                parent.removeView(ttv);
-                parent.addView(ttv, 0);
-
-                ttv.setSurfaceTexture(output.getSurfaceTexture());
-                updateTransform();
+//                ViewGroup parent = (ViewGroup) ttv.getParent();
+//                parent.removeView(ttv);
+//                parent.addView(ttv, 0);
+//
+//                ttv.setSurfaceTexture(output.getSurfaceTexture());
+//
+//                updateTransform();
+                Log.d("main", "onUpdated ");
             }
         });
 
@@ -116,18 +129,20 @@ public class MainActivity extends AppCompatActivity {
     private void updateTransform() {
         Matrix matrix = new Matrix();
 
-        // Compute the center of the view finder
-        float centerX = ttv.getWidth() / 2f;
-        float centerY = ttv.getHeight() / 2f;
-
-        float[] rotations = {0,90,180,270};
-        // Correct preview output to account for display rotation
-        float rotationDegrees = rotations[ttv.getDisplay().getRotation()];
-
-        matrix.postRotate(-rotationDegrees, centerX, centerY);
-
-        // Finally, apply transformations to our TextureView
-        ttv.setTransform(matrix);
+//        // Compute the center of the view finder
+//        float centerX = ttv.getWidth() / 2f;
+//        float centerY = ttv.getHeight() / 2f;
+//
+//        float[] rotations = {0,90,180,270};
+//        // Correct preview output to account for display rotation
+//        float rotationDegrees = rotations[ttv.getDisplay().getRotation()];
+//
+////        matrix.postScale(-1, 1);
+//        matrix.postRotate(90, centerX, centerY);
+//
+//
+//        // Finally, apply transformations to our TextureView
+//        ttv.setTransform(matrix);
     }
 
     @Override
@@ -168,8 +183,74 @@ public class MainActivity extends AppCompatActivity {
             final Image img = image.getImage();
             if (img != null) {
                 Log.d("MainActivity", "Image Time Stamp is: " + img.getTimestamp());
+                Bitmap bitmap = toBitmap(img);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ttv.setImageBitmap(bitmap);
+                    }
+                });
+//                ByteBuffer buffer = img.getPlanes()[0].getBuffer();
+//                byte[] bytes = new byte[buffer.capacity()];
+//                buffer.get(bytes);
+//                Bitmap bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+
             }
         }
+    }
+
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        byte[] rotatenv21 = rotateYUV420Degree90(nv21, image.getWidth(), image.getHeight());
+
+        YuvImage yuvImage = new YuvImage(rotatenv21, ImageFormat.NV21, image.getHeight(), image.getWidth(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
+    private byte[] rotateYUV420Degree90(byte[] data, int imageWidth, int imageHeight)
+    {
+        byte [] yuv = new byte[imageWidth*imageHeight*3/2];
+        // Rotate the Y luma
+        int i = 0;
+        for(int x = 0;x < imageWidth;x++)
+        {
+            for(int y = imageHeight-1;y >= 0;y--)
+            {
+                yuv[i] = data[y*imageWidth+x];
+                i++;
+            }
+        }
+        // Rotate the U and V color components
+        i = imageWidth*imageHeight*3/2-1;
+        for(int x = imageWidth-1;x > 0;x=x-2)
+        {
+            for(int y = 0;y < imageHeight/2;y++)
+            {
+                yuv[i] = data[(imageWidth*imageHeight)+(y*imageWidth)+x];
+                i--;
+                yuv[i] = data[(imageWidth*imageHeight)+(y*imageWidth)+(x-1)];
+                i--;
+            }
+        }
+        return yuv;
     }
 
 }
